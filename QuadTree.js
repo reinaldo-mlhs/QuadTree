f644906c828336898b7e58dd266d4854eff99371
Reinaldo Magalhaes <reinaldomagalhaes96@gmail.com>
2023-11-26 09:01:02 -0600

    add capacity option and check if point is in range of radius

--- QuadTree.js
+++ QuadTree.js
@@ -1,14 +1,14 @@
 class Boundary {
-    constructor(center, halfDimension) {
+    constructor(center, radius) {
         this.center = center;
-        this.halfDimension = halfDimension;
+        this.radius = radius;
     }
 
-    containsPoint(point) {
-        const x1 = this.center.x - this.halfDimension;
-        const y1 = this.center.y - this.halfDimension;
-        const x2 = this.center.x + this.halfDimension;
-        const y2 = this.center.y + this.halfDimension;
+    containsPointRect(point) {
+        const x1 = this.center.x - this.radius;
+        const y1 = this.center.y - this.radius;
+        const x2 = this.center.x + this.radius;
+        const y2 = this.center.y + this.radius;
 
         if (point.x >= x1 && point.x < x2) {
             if (point.y >= y1 && point.y < y2) {
@@ -19,16 +19,26 @@ class Boundary {
         return false;
     }
 
-    intersectsRange(range) {
-        const aX1 = range.center.x - range.halfDimension;
-        const aY1 = range.center.y - range.halfDimension;
-        const aX2 = range.center.x + range.halfDimension;
-        const aY2 = range.center.y + range.halfDimension;
+    containsPointCircle(point) {
+        const distance = Math.sqrt((Math.pow(point.x - this.center.x, 2)) + (Math.pow(point.y - this.center.y, 2)));
 
-        const bX1 = this.center.x - this.halfDimension;
-        const bY1 = this.center.y - this.halfDimension;
-        const bX2 = this.center.x + this.halfDimension;
-        const bY2 = this.center.y + this.halfDimension;
+        if (distance <= this.radius) {
+            return true;
+        }
+
+        return false;
+    }
+
+    intersectsRangeRect(range) {
+        const aX1 = range.center.x - range.radius;
+        const aY1 = range.center.y - range.radius;
+        const aX2 = range.center.x + range.radius;
+        const aY2 = range.center.y + range.radius;
+
+        const bX1 = this.center.x - this.radius;
+        const bY1 = this.center.y - this.radius;
+        const bX2 = this.center.x + this.radius;
+        const bY2 = this.center.y + this.radius;
 
         if (aX1 < bX2 && aX2 > bX1 && aY1 < bY2 && aY2 > bY1) {
             return true;
@@ -36,11 +46,12 @@ class Boundary {
 
         return false;
     }
+
 }
 
 class QuadTree {
-    constructor(boundary) {
-        this.capacity = 8;
+    constructor(boundary, capacity = 8) {
+        this.capacity = capacity;
         this.boundary = boundary;
         this.entities = [];
 
@@ -54,7 +65,7 @@ class QuadTree {
 
         const point = entity.point;
 
-        if (!this.boundary.containsPoint(point)) {
+        if (!this.boundary.containsPointRect(point)) {
             return false;
         }
 
@@ -84,30 +95,30 @@ class QuadTree {
     }
 
     subdivide() {
-        const northWestCenter = {x: this.boundary.center.x - (this.boundary.halfDimension / 2), y: this.boundary.center.y - (this.boundary.halfDimension / 2)}
-        this.northWest = new QuadTree(new Boundary(northWestCenter, this.boundary.halfDimension / 2));
+        const northWestCenter = {x: this.boundary.center.x - (this.boundary.radius / 2), y: this.boundary.center.y - (this.boundary.radius / 2)}
+        this.northWest = new QuadTree(new Boundary(northWestCenter, this.boundary.radius / 2));
 
-        const northEastCenter = {x: this.boundary.center.x + (this.boundary.halfDimension / 2), y: this.boundary.center.y - (this.boundary.halfDimension / 2)}
-        this.northEast = new QuadTree(new Boundary(northEastCenter, this.boundary.halfDimension / 2));
+        const northEastCenter = {x: this.boundary.center.x + (this.boundary.radius / 2), y: this.boundary.center.y - (this.boundary.radius / 2)}
+        this.northEast = new QuadTree(new Boundary(northEastCenter, this.boundary.radius / 2));
 
-        const southWestCenter = {x: this.boundary.center.x - (this.boundary.halfDimension / 2), y: this.boundary.center.y + (this.boundary.halfDimension / 2)}
-        this.southWest = new QuadTree(new Boundary(southWestCenter, this.boundary.halfDimension / 2));
+        const southWestCenter = {x: this.boundary.center.x - (this.boundary.radius / 2), y: this.boundary.center.y + (this.boundary.radius / 2)}
+        this.southWest = new QuadTree(new Boundary(southWestCenter, this.boundary.radius / 2));
 
-        const southEastCenter = {x: this.boundary.center.x + (this.boundary.halfDimension / 2), y: this.boundary.center.y + (this.boundary.halfDimension / 2)}
-        this.southEast = new QuadTree(new Boundary(southEastCenter, this.boundary.halfDimension / 2));
+        const southEastCenter = {x: this.boundary.center.x + (this.boundary.radius / 2), y: this.boundary.center.y + (this.boundary.radius / 2)}
+        this.southEast = new QuadTree(new Boundary(southEastCenter, this.boundary.radius / 2));
     }
 
-    queryRange(range) {
+    queryRangeRect(range) {
 
         const entitiesInRange = [];
 
-        if (!this.boundary.intersectsRange(range)) {
+        if (!this.boundary.intersectsRangeRect(range)) {
             return entitiesInRange;
         }
 
         this.entities.forEach(entity => {
             
-            if (range.containsPoint(entity.point)) {
+            if (range.containsPointRect(entity.point)) {
                 entitiesInRange.push(entity);
             }
         })
@@ -116,10 +127,38 @@ class QuadTree {
             return entitiesInRange;
         }
         
-        entitiesInRange.push(this.northWest.queryRange(range));
-        entitiesInRange.push(this.northEast.queryRange(range));
-        entitiesInRange.push(this.southWest.queryRange(range));
-        entitiesInRange.push(this.southEast.queryRange(range));
+        entitiesInRange.push(this.northWest.queryRangeRect(range));
+        entitiesInRange.push(this.northEast.queryRangeRect(range));
+        entitiesInRange.push(this.southWest.queryRangeRect(range));
+        entitiesInRange.push(this.southEast.queryRangeRect(range));
+
+        return entitiesInRange.flat();
+
+    }
+
+    queryRangeCircle(range) {
+
+        const entitiesInRange = [];
+
+        if (!this.boundary.intersectsRangeRect(range)) {
+            return entitiesInRange;
+        }
+
+        this.entities.forEach(entity => {
+            
+            if (range.containsPointCircle(entity.point)) {
+                entitiesInRange.push(entity);
+            }
+        })
+
+        if (this.northWest === null) {
+            return entitiesInRange;
+        }
+        
+        entitiesInRange.push(this.northWest.queryRangeCircle(range));
+        entitiesInRange.push(this.northEast.queryRangeCircle(range));
+        entitiesInRange.push(this.southWest.queryRangeCircle(range));
+        entitiesInRange.push(this.southEast.queryRangeCircle(range));
 
         return entitiesInRange.flat();
 
